1ed6d61890c4443cac08a0589438ff81d82f57b8
robot156 <gim162913@gmail.com>
2026-05-01 15:03:34 +0900

    feat: 피쳐 모듈 자동 생성 플러그인 추가

--- build-logic/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,5 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.moim.convention.GenerateFeatureModuleTask
 import com.moim.convention.Plugins
 import com.moim.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
@@ -21,6 +22,25 @@ internal class AndroidApplicationConventionPlugin : Plugin<Project> {
                     versionName = MoimConfig.VERSION_NAME
                 }
             }
+
+            registerGenerateFeatureModuleTask(rootProject)
+        }
+    }
+
+    private fun registerGenerateFeatureModuleTask(rootProject: Project) {
+        if (rootProject.tasks.findByName("generateFeatureModule") != null) return
+
+        val featureNameProvider = rootProject.providers.gradleProperty("featureName")
+        val templateRootDirectory = rootProject.layout.projectDirectory.dir("templates/feature-module")
+        val settingsGradleFile = rootProject.layout.projectDirectory.file("settings.gradle.kts")
+        val featuresRootDirectory = rootProject.layout.projectDirectory.dir("feature")
+
+        rootProject.tasks.register("generateFeatureModule", GenerateFeatureModuleTask::class.java) {
+            featureName.set(featureNameProvider)
+            basePackageName.set("com.moim.feature")
+            this.templateRootDirectory.set(templateRootDirectory)
+            outputDirectory.set(featureNameProvider.map { name -> featuresRootDirectory.dir(name) })
+            this.settingsGradleFile.set(settingsGradleFile)
         }
     }
 }
